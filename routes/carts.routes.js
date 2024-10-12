ae4c241414e21bd8d1f185951a80f7c334b087e3
Anas Moner <ansmoner2000@gmail.com>
2024-10-12 18:16:20 +0300

    End CouponCode and Cart without testing on postman.

--- routes/carts.routes.js
+++ routes/carts.routes.js
@@ -2,10 +2,19 @@ const express = require("express");
 const router = express.Router();
 const cartController = require("../controllers/cart.controller");
 
-router.post("/", cartController.addToCart);
+// Add item to cart
+router.post("/create", cartController.addToCart);
+
+// Update cart item
 router.put("/:cartId", cartController.updateCart);
-router.delete("/:cartId", cartController.removeFromCart);
-router.get("/user/:userId", cartController.getCart);
-router.delete("/user/:userId", cartController.clearCart);
+
+// Remove item from cart by productId and userId
+router.delete("/:productId/:userId", cartController.removeFromCart);
+
+// Get all cart items for a specific user
+router.get("/:userId", cartController.getCart);
+
+// Clear entire cart for a specific user
+router.delete("/clear/:userId", cartController.clearCart);
 
 module.exports = router;
