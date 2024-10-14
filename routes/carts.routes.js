49ec275db96b4a1fef4d056eb4cad0965f2ed32d
Anas Moner <ansmoner2000@gmail.com>
2024-10-14 23:26:57 +0300

    Add Cart.txt

--- routes/carts.routes.js
+++ routes/carts.routes.js
@@ -3,16 +3,13 @@ const router = express.Router();
 const cartController = require("../controllers/cart.controller");
 
 // Add item to cart
-router.post('/add', cartController.addToCart);
+router.post('/', cartController.addToCart);
 
 // Update cart item
 router.put('/update/:cartId', cartController.updateCart);
 
 // Remove product(s) from the cart
 router.delete('/remove/:userId', cartController.removeProductsFromCart);
-
-
-
 
 // Get cart items for a specific user
 router.get('/:userId', cartController.getCart);
