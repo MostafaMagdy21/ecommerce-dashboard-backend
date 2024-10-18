331d818764b1bfd44c6ca8230c15a7780278036f
Anas Moner <ansmoner2000@gmail.com>
2024-10-18 04:34:44 +0300

    update categories

--- middlewares/categories.middleware.js
+++ middlewares/categories.middleware.js
@@ -18,19 +18,3 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 module.exports = { upload: upload.array("files", 5) };
-
-
-// app.post("/categories", upload.array("files", 5), (req, res) => {
-//   try {
-//     const formData = req.body;
-//     const files = req.files;
-
-//     // Process the form data and files here
-//     console.log("Form data:", formData);
-//     console.log("Uploaded files:", files);
-//     return res.status(200).json({ message: "Form submitted successfully" });
-//   } catch (error) {
-//     console.error("Error processing form:", error);
-//     res.status(500).json({ message: "Error processing form" });
-//   }
-// });
